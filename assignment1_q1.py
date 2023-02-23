8f10e7372e5f7de36471cff55681c7ae49a653b5
vikasgithub1122 <109681282+vikasgithub1122@users.noreply.github.com>
2023-02-23 23:37:31 +0530

    Update assignment1_q1.py

--- assignment1_q1.py
+++ assignment1_q1.py
@@ -1,15 +1,6 @@
 num = int(input("How many terms you want 'fibonacci sequence': "))
 
 n1, n2 = 0, 1 # first two number
-
-
-if num< 0:
-   print("Please enter a positive integer")
-
-elif num == 1 & num==0:
-   print("Fibonacci sequence upto",num,":")
-   print(n1)
-   
 else:
    print("Fibonacci sequence:")
    while n2<num:
